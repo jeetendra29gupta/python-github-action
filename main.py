c1c41f802ad32781c417ac4619c40b32cb05023c
Jeetendra Gupta <Jeetendra.rimt@gmail.com>
2025-08-12 13:02:49 +0530

    My Github Action for formatting my code

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import datetime
 
 
 def main():
-    print(
-        f"Hello from GitHub Actions! Today's date and time is: {datetime.datetime.now()}"
-    )
+    now = datetime.datetime.now()
+    print(f"Hello from GitHub Actions! Today's date and time is: {now}")
 
 
 if __name__ == "__main__":
